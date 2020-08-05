c4a21865d13ad7bc099b16a6128ccc53bc01e389
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2020-08-04 19:21:22 -0500

    Setup project. Add imagens to project.

---
+++ Pachinko/Pachinko/GameScene.swift
@@ -0,0 +1,32 @@
+//
+//  GameScene.swift
+//  Pachinko
+//
+//  Created by Álvaro Ávalos Hernández on 04/08/20.
+//  Copyright © 2020 Álvaro Ávalos Hernández. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            let box = SKSpriteNode(color: UIColor.red, size: CGSize(width: 64, height: 64))
+            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+            box.position = location
+            addChild(box)
+        }
+    }
+    
+}
